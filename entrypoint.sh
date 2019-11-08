4927bf85c58645c0314fc4afd37704b120c3f2b2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-11-08 08:15:40 +0100

    correct entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ $GALAXY_VIRTUAL_ENV/bin/uwsgi \
   --thunder-lock \
   --master \
   --logto $GALAXY_LOGS_DIR/uwsgi_log \
-  --http :8080 --pythonpath lib \
-  --module galaxy.webapps.galaxy.buildapp:uwsgi_app \
+  --http :8080 \
+  --pythonpath lib \
+  --virtualenv $GALAXY_VIRTUAL_ENV \
+  --module 'galaxy.webapps.galaxy.buildapp:uwsgi_app()' \
   -b 16384
